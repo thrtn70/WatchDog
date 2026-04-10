c221a65fa52fbe1aba9f11120278ae599596721b
thrtn70 <xuanthong.thornton@gmail.com>
2026-04-09 20:55:07 -0700

    fix: add missing System.Reflection using for GetCustomAttribute

--- src/WatchDog.App/ViewModels/MainWindowViewModel.cs
+++ src/WatchDog.App/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
